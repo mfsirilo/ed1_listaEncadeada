2d0ad92dd5190d65e4a93896fe6da7c97d780bff
marcos <marcos@Vostro-3450>
2018-05-03 09:27:14 -0300

    O inicio

---
+++ src/listaencadeada/ListaEncadeada.java
@@ -0,0 +1,37 @@
+package listaencadeada;
+
+public class ListaEncadeada {
+
+    private No primeiro;
+    private No ultimo;
+    private int totalDeElementos;
+
+    public void adiciona(Object elemento) {
+
+    }
+
+    public void adiciona(int posicao, Object elemento) {
+
+    }
+    
+    public void remove (int posicao){
+        
+    }
+    
+    public int tamanho (){return 0;}
+    public boolean contem (Object o){return false;}
+    public void adicionaNoComeco(Object elemento){
+        
+    }
+    public void removeDoComeco(){
+        
+    }
+    public void removeDoFim(){
+        
+    }
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
---
+++ src/listaencadeada/No.java
@@ -0,0 +1,32 @@
+package listaencadeada;
+public class No {
+    private No proximo;
+    private Object elemento;
+
+    
+    public No (No proximo, Object elemento){
+        this.proximo = proximo;
+        this.elemento = elemento;
+    }
+    
+    public No (Object elemento){
+        this.elemento = elemento;
+    }
+    
+    public No getProximo() {
+        return proximo;
+    }
+
+    public void setProximo(No proximo) {
+        this.proximo = proximo;
+    }
+
+    public Object getElemento() {
+        return elemento;
+    }
+
+    public void setElemento(Object elemento) {
+        this.elemento = elemento;
+    }
+    
+}
